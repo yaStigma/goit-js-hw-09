9b8b84f9ff9737ec12c9337c1ae44dc748887729
Tanya Yenakieva <yastigma@gmail.com>
2023-11-18 21:38:51 +0200

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.body,
+btnStart: document.querySelector("[data-start]"),
+btnStop: document.querySelector("[data-stop]"),
+}
+let timerId = null;
 
+refs.btnStart.addEventListener("click", () => {
+if (!timerId) {
+        refs.btnStart.disabled = true;
+
+        timerId = setInterval(() => {
+            const randomColor = getRandomHexColor();
+            refs.body.style.backgroundColor = randomColor;
+        }, 1000);
+    }
+});
+
+refs.btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+      timerId = null;
+    refs.btnStart.disabled = false;
+  });
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+//         
