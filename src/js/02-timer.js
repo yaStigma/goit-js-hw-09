ab1712026e5d4df206b8945a4843d557fbc26f48
Tanya Yenakieva <yastigma@gmail.com>
2023-11-19 21:56:22 +0200

    task 2 completed
    залишилось додати бібліотеку для сповіщень

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,30 +1,56 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-refs = {
+const refs = {
     inputElem: document.querySelector("#datetime-picker"),
     btnStart: document.querySelector("[data-start]"),
     daysElem: document.querySelector("[data-days]"),
-    hoursEleme: document.querySelector("[data-hours]"),
+    hoursElem: document.querySelector("[data-hours]"),
     minutesElem: document.querySelector("[data-minutes]"),
     secondsElem: document.querySelector("[data-seconds]"),
 };
-const datetimePicker = new flatpickr(inputElem);
 
+let selectedDate;
+
+refs.btnStart.disabled = true;
   
     const options = {
-        enableTime: true,
-        time_24hr: true,
-        defaultDate: new Date(),
-        minuteIncrement: 1,
+        enableTime: true, //можливість вибору часу
+        time_24hr: true,  // 24 години або 12 pm am
+        defaultDate: new Date(), // встанослює початкову дату або обрану дату 
+        minuteIncrement: 1, // крок вводу хвилин
         onClose(selectedDates) {
-            console.log(selectedDates[0]);
-        },
+          const selectedDate = selectedDates[0];
+
+        if (selectedDate <= new Date()) {
+      window.alert("Please choose a date in the future");
+    } else {
+      refs.btnStart.disabled = false;
+      selectedDate.setSeconds(0);
+    }
+        console.log(selectedDate);
+    },  //функція, яка спрацьовує кожен раз при закритті календарю. виводить значення у консоль
     }
 
+flatpickr("#datetime-picker", options);
+
+refs.btnStart.addEventListener("click", () => {
+  selectedDate = refs.inputElem._flatpickr.selectedDates[0];
+   const timerInterval = setInterval(updateTimer, 1000);
+
+  function updateTimer() {
+    const ms = selectedDate - new Date();
+    if (ms <= 0) {
+      clearInterval(timerInterval);
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(ms);
+      renderTimer({ days, hours, minutes, seconds });
+    }
+  }
+});
 
 
-    function convertMs(ms) {
+function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -40,5 +66,18 @@ const datetimePicker = new flatpickr(inputElem);
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+
   return { days, hours, minutes, seconds };
-};
+}
+
+
+function renderTimer({ days, hours, minutes, seconds }) {
+  refs.daysElem.textContent = addLeadingZero(days);
+  refs.hoursElem.textContent = addLeadingZero(hours);
+  refs.minutesElem.textContent = addLeadingZero(minutes);
+  refs.secondsElem.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
